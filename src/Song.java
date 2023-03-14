f2a458a07be3c8f64c2522fead940f593713e32f
nickfinbo <nicholasjfcd2023@student.cis.edu.hk>
2023-03-14 08:40:24 +0800

    done

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> Listened = new ArrayList<>();
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> listeners) {
+        int newListeners = 0;
+
+        for (int i = 0; i < listeners.size(); i++) {
+            String currListener = listeners.get(i).toLowerCase();
+
+            if(!Listened.contains(currListener)) {
+                Listened.add(currListener);
+                newListeners++;
+            }
+        }
+
+        return newListeners;
+    }
 }
